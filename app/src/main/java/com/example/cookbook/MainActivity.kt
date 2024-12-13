5851134779116835246bfc01992edc3e84751dcb
caitlin3862 <caitlinl21@nycstudents.net>
2024-12-13 00:22:28 -0500

    edited first page (will change layout later)

--- app/src/main/java/com/example/cookbook/MainActivity.kt
+++ app/src/main/java/com/example/cookbook/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.cookbook
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfig: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val appBar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(appBar)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val navGraph: NavGraph = navController.graph
+        appBarConfig = AppBarConfiguration(navGraph)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val success: Boolean = navController.navigateUp(appBarConfig)
+        return success || super.onSupportNavigateUp()
     }
 }
